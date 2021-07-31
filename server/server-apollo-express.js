465636430af7ce40ce4deafdd4a41b6fcc7d72a1
YiroYi <yiroyi@live.com>
2021-07-31 19:47:48 +0900

    Adding 3 server, node, apollo-express, apoll

--- server/server-apollo-express.js
+++ server/server-apollo-express.js
@@ -1,8 +1,16 @@
-const { ApolloServer, gql } = require("apollo-server");
+const express = require("express");
+const app = express();
+
 const mongoose = require("mongoose");
-const User = require("./models/user");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
+const User = require("./models/user");
+
+app.use(cors());
 dotenv.config();
+
+const { ApolloServer, gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Query {
@@ -60,18 +68,26 @@ const resolvers = {
   },
 };
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
 const db = process.env.DB_URL;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
 const PORT = process.env.PORT || 5000;
-mongoose.connect(db,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    server.listen(PORT,()=>{
-        console.log(`Running running on port ${PORT}`)
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Running running on port ${PORT}`);
     });
-}).catch( err => {
-    console.log(err)
-});
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ server/server-express.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const { graphqlHTTP } = require("express-graphql");
+const bodyParser = require("body-parser");
+const { buildSchema } = require("graphql");
+const expressPlayground =
+  require("graphql-playground-middleware-express").default;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require('cors')
+const User = require("./models/user");
+
+app.use(cors())
+dotenv.config();
+
+app.use(bodyParser.json());
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+            type RootQuery {
+              user(id:ID!): User!
+            }
+            type RootMutation{
+              addUser(userInput: UserInput!): User!
+            }
+
+            type User {
+              _id: ID!
+              email: String!
+              password: String!
+            }
+
+            input UserInput {
+              email: String!
+              password: String!
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+    rootValue: {
+      user: async (args) => {
+        try {
+          const user = await User.findOne({ _id: args.id });
+
+          return {
+            ...user._doc,
+          };
+        } catch (err) {
+          throw err;
+        }
+      },
+      addUser: async (args) => {
+        try {
+          const user = new User({
+            email: args.userInput.email,
+            password: args.userInput.password,
+          });
+
+          const result = await user.save();
+          console.log("IM MONGOOSE RESULT");
+          console.log(result);
+
+          return {
+            ...result._doc,
+          };
+        } catch (error) {
+          throw error;
+        }
+      },
+    },
+    graphiql: true,
+  })
+);
+
+const db = process.env.DB_URL;
+
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Running running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
