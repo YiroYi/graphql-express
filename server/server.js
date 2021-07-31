edb2cd555d889ae990cfd45d026613ce7784ca0e
YiroYi <yiroyi@live.com>
2021-07-31 18:15:19 +0900

    Adding Moongose conection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { graphqlHTTP } = require('express-graphql');
-const bodyParser = require('body-parser');
-const { buildSchema } = require('graphql');
-const expressPlayground = require('graphql-playground-middleware-express').default
+const { graphqlHTTP } = require("express-graphql");
+const bodyParser = require("body-parser");
+const { buildSchema } = require("graphql");
+const expressPlayground = require("graphql-playground-middleware-express").default;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+ 
 
+
+dotenv.config();
 
 app.use(bodyParser.json());
-app.get('/playground',expressPlayground({endpoint: '/graphql'}))
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
 app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema:buildSchema(`
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
             type RootQuery {
                 hello: String!
             }
@@ -24,17 +29,28 @@ app.use(
                 mutation: RootMutation
             }
         `),
-        rootValue:{
-            hello:()=>{
-                return 'Hello back!!'
-            }
-        },
-        graphiql: true
-    })
-)
+    rootValue: {
+      hello: () => {
+        return "Hello back!!";
+      },
+    },
+    graphiql: true,
+  })
+);
 
+const db = process.env.DB_URL;
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Running running on port ${PORT}`)
-});
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Running running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
